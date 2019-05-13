0a4360fa163eb9c1665c8c42242452f26afcca51
Pham Gia Trung <giatrung1998pro@gmail.com>
2019-05-13 10:28:22 +0700

    BaiBasicView

---
+++ app/src/main/java/com/example/baibasicview/BasicView.java
@@ -0,0 +1,25 @@
+package com.example.baibasicview;
+
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+
+public class BasicView extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_view);
+    }
+}
